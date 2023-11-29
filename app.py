52e17a181c24bb3e63ef5713f81be7ecec724f03
Princess <grace.caballeroalumisin@gmail.com>
2023-11-29 18:07:14 +0800

    Modified MEDetect

--- app.py
+++ app.py
@@ -1,16 +1,27 @@
-from flask import Flask, render_template, Response, jsonify, request, session
+from flask import Flask, render_template, Response, jsonify, request, session, flash
 import os
 from model import process_image
 import json 
+from flask_mail import Mail, Message
+from config import mail_username, mail_password
+
 
 #app configs
 app = Flask(__name__)   
 app.config['SECRET_KEY'] = os.urandom(24)
-app.config['UPLOAD_FOLDER'] = 'static/files'
+app.config['UPLOAD_FOLDER'] = 'static/assets/files'
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = mail_username
+app.config['MAIL_PASSWORD'] = mail_password
 
 #model paths
 MODEL_AUG_PATH = 'medetect_withaugment.pt'
 MODEL_NO_AUG_PATH = 'medetect_withoutaugment.pt'
+
+#mail
+mail = Mail(app)
 
 #index route
 @app.route("/")
@@ -46,3 +57,19 @@ def upload():
             result = process_image(mode, file, app.config['UPLOAD_FOLDER'])
 
             return jsonify({'message': 'success', 'success': True, 'file_path': result})
+
+#contact form submission
+@app.route("/contact-form", methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        email = request.form.get('email')
+        message = request.form.get('message')
+
+        msg = Message(subject=f"Mail from {name}", body=f"Name: {name}\nEmail: {email}\n\n{message}", 
+                      sender = mail_username, recipients = ['MEDetect2023@gmail.com'])
+        mail.send(msg)
+        
+        return render_template('index.jinja', success = True)
+         
+    return render_template('index.jinja')
